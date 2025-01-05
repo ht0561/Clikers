460d90513114a8f36112c18ee42c26ea4578f1db
hanna <ht0561@bs.amlnet.k12.il>
2025-01-05 22:51:38 +0200

    task1

--- app/src/main/java/com/example/clikers/MainActivity.java
+++ app/src/main/java/com/example/clikers/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.clikers;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button sarbach;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        sarbach= (Button) findViewById(R.id.button2 );
+
+    }
+
+    public void go(View view) {
+
+        sarbach.setText ("Oh, yea, I’ve been clicked!");
+
     }
 }
